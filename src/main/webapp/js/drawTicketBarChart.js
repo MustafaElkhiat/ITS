7946eaac779af97209bc1ae4eb979dc319af567b
Mustafa <melkhiat@gmail.com>
2020-08-09 16:37:15 +0200

    Delete dick number from bar chart

--- src/main/webapp/js/drawTicketBarChart.js
+++ src/main/webapp/js/drawTicketBarChart.js
@@ -22,7 +22,7 @@ var initTicketBarChart = function () {
     var options = {
         height: 300,
         vAxis: {
-            ticks: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+
             textStyle: {
                 bold: true,
             }
